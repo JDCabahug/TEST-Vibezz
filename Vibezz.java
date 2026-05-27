4db24b99970dc2aaf40c190af81be032368f211e
Jerry Dean II A. Cabahug <jerrydeanabata.cabahug@gmail.com>
2026-05-27 21:47:23 +0800

    Comment out audio playback testing code
    Commented out audio playback test code for future reference.

--- Vibezz.java
+++ Vibezz.java
@@ -2,9 +2,29 @@ import Main.Panel;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.JFrame;
 
+/*
+//for testing the audio playback
+import ControlPanel.MusicHandler;
+import ControlPanel.Playlist;
+import ControlPanel.Song;
+*/
+
 public class Vibezz{
     public static void main(String [] args){
-
+        /*
+        //audio playback test
+        MusicHandler music = new MusicHandler();
+        //To make a list 
+        for (Playlist p : music.getPlaylists()) {
+            System.out.println("Playlist: " + p.getName());
+            for (Song s : p.getSongs()) {
+                System.out.println("  - " + s.getTitle() + " by " + s.getArtist());
+            }
+        }
+        // Test Play (1st get: playlist index, 2nd: song index within the playlist)
+        music.play(music.getPlaylists().get(1).getSongs().get(0)); 
+        */
+        
         JFrame jframe = new JFrame();
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setResizable(false);
